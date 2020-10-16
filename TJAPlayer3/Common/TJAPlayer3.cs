27eaa833f066936fe7201738d6cbe8397bead120
KabanFriends <yuukun2004@protonmail.ch>
2020-10-16 21:36:06 +0900

    ビルドバージョン表示

--- TJAPlayer3/Common/TJAPlayer3.cs
+++ TJAPlayer3/Common/TJAPlayer3.cs
@@ -19,14 +19,16 @@ namespace TJAPlayer3
 {
 	internal class TJAPlayer3 : Game
 	{
-        // プロパティ
-        #region [ properties ]
-        public static readonly string VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString().Substring(0, Assembly.GetExecutingAssembly().GetName().Version.ToString().Length - 2);
+		// プロパティ
+		#region [ properties ]
+		public static readonly string VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         public static readonly string SLIMDXDLL = "c_net20x86_Jun2010";
-		public static readonly string D3DXDLL = "d3dx9_43.dll";		// June 2010
-        //public static readonly string D3DXDLL = "d3dx9_42.dll";	// February 2010
-        //public static readonly string D3DXDLL = "d3dx9_41.dll";	// March 2009
+		public static readonly string D3DXDLL = "d3dx9_43.dll";     // June 2010
+																	//public static readonly string D3DXDLL = "d3dx9_42.dll";	// February 2010
+																	//public static readonly string D3DXDLL = "d3dx9_41.dll";	// March 2009
+
+		public static readonly string AppDisplayName = Assembly.GetExecutingAssembly().GetName().Name;
 
 		public static TJAPlayer3 app
 		{
@@ -2302,7 +2304,7 @@ for (int i = 0; i < 3; i++) {
             #endregion
 
             #region Discordの処理
-            Discord.Initialize("753220258961293373");
+            Discord.Initialize("764870299879079947");
             StartupTime = Discord.GetUnixTime();
             Discord.UpdatePresence("", Properties.Discord.Stage_StartUp, StartupTime);
             #endregion
@@ -2338,7 +2340,7 @@ for (int i = 0; i < 3; i++) {
 				delay = "(" + Sound管理.GetSoundDelay() + "ms)";
 			}
             AssemblyName asmApp = Assembly.GetExecutingAssembly().GetName();
-            base.Window.Text = asmApp.Name + " Ver." + VERSION + " (" + Sound管理.GetCurrentSoundDeviceType() + delay + ")";
+            base.Window.Text = asmApp.Name + " - Build " + VERSION + " (" + Sound管理.GetCurrentSoundDeviceType() + delay + ")";
 		}
 
 		private void t終了処理()
