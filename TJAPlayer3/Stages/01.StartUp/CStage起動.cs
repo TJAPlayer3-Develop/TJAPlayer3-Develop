051bd3f961b7fc257a29245610a91556f0cd9b4f
KabanFriends <yuukun2004@protonmail.ch>
2020-10-17 12:51:17 +0900

    起動画面の文字描画コードを削除

--- TJAPlayer3/Stages/01.StartUp/CStage起動.cs
+++ TJAPlayer3/Stages/01.StartUp/CStage起動.cs
@@ -80,91 +80,31 @@ namespace TJAPlayer3
 				base.OnManagedリソースの解放();
 			}
 		}
+		public void t読込開始()
+		{
+			this.list進行文字列.Add("TEST");
+
+			es = new CEnumSongs();
+			es.StartEnumFromCache(); // 曲リスト取得(別スレッドで実行される)
+		}
+
 		public override int On進行描画()
 		{
 			if( !base.b活性化してない )
 			{
 				if( base.b初めての進行描画 )
 				{
-					this.list進行文字列.Add( "DTXManiaXG Ver.K powered by YAMAHA Silent Session Drums\n" );
-					this.list進行文字列.Add( "Product by.kairera0467\n" );
-					this.list進行文字列.Add( "Release: " + TJAPlayer3.VERSION + " [" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + "]" );
-
-                    this.list進行文字列.Add("");
-                    this.list進行文字列.Add("TJAPlayer3 forked TJAPlayer2 forPC(kairera0467)");
-                    this.list進行文字列.Add("TJAPlayer3 edited by AioiLight(@aioilight)");
-		    this.list進行文字列.Add("");
-		    this.list進行文字列.Add("TJAPlayer3-Develop initiated by touhou-renren(@ren43723591)");
-                    this.list進行文字列.Add("");
-
-                    es = new CEnumSongs();
-					es.StartEnumFromCache();										// 曲リスト取得(別スレッドで実行される)
+					this.t読込開始();
 					base.b初めての進行描画 = false;
-					return 0;
 				}
-
-				// CSongs管理 s管理 = CDTXMania.Songs管理;
-
-				//if( this.tx背景 != null )
-				//	this.tx背景.t2D描画( CDTXMania.app.Device, 0, 0 );
 
 				#region [ this.str現在進行中 の決定 ]
 				//-----------------
 				switch( base.eフェーズID )
 				{
-					case CStage.Eフェーズ.起動0_システムサウンドを構築:
-						this.str現在進行中 = "SYSTEM SOUND...";
-						break;
-
-					case CStage.Eフェーズ.起動00_songlistから曲リストを作成する:
-						this.str現在進行中 = "SONG LIST...";
-						break;
-
-					case CStage.Eフェーズ.起動1_SongsDBからスコアキャッシュを構築:
-						this.str現在進行中 = "SONG DATABASE...";
-						break;
-
-					case CStage.Eフェーズ.起動2_曲を検索してリストを作成する:
-						this.str現在進行中 = string.Format( "{0} ... {1}", "Enumerating songs", es.Songs管理.n検索されたスコア数 );
-						break;
-
-					case CStage.Eフェーズ.起動3_スコアキャッシュをリストに反映する:
-						this.str現在進行中 = string.Format( "{0} ... {1}/{2}", "Loading score properties from songs.db", es.Songs管理.nスコアキャッシュから反映できたスコア数, es.Songs管理.n検索されたスコア数 );
-						break;
-
-					case CStage.Eフェーズ.起動4_スコアキャッシュになかった曲をファイルから読み込んで反映する:
-						this.str現在進行中 = string.Format( "{0} ... {1}/{2}", "Loading score properties from files", es.Songs管理.nファイルから反映できたスコア数, es.Songs管理.n検索されたスコア数 - es.Songs管理.nスコアキャッシュから反映できたスコア数 );
-						break;
-
-					case CStage.Eフェーズ.起動5_曲リストへ後処理を適用する:
-						this.str現在進行中 = string.Format( "{0} ... ", "Building songlists" );
-						break;
-
-					case CStage.Eフェーズ.起動6_スコアキャッシュをSongsDBに出力する:
-						this.str現在進行中 = string.Format( "{0} ... ", "Saving songs.db" );
-						break;
-
 					case CStage.Eフェーズ.起動7_完了:
-                        this.list進行文字列.Add("LOADING TEXTURES...");
                         TJAPlayer3.Tx.LoadTexture();
-                        this.list進行文字列.Add("LOADING TEXTURES...OK");
-                        this.str現在進行中 = "Setup done.";
                         break;
-				}
-				//-----------------
-				#endregion
-				#region [ this.list進行文字列＋this.現在進行中 の表示 ]
-				//-----------------
-				lock( this.list進行文字列 )
-				{
-					int x = 320;
-					int y = 20;
-					foreach( string str in this.list進行文字列 )
-					{
-						TJAPlayer3.act文字コンソール.tPrint( x, y, C文字コンソール.Eフォント種別.白, str );
-						y += 24;
-					}
-					TJAPlayer3.act文字コンソール.tPrint( x, y, C文字コンソール.Eフォント種別.白, this.str現在進行中 );
 				}
 				//-----------------
 				#endregion
@@ -187,6 +127,8 @@ namespace TJAPlayer3
 		private CTexture tx背景;
 		private CEnumSongs es;
 
+		private bool bLoadStarted;
+
 #if false
 		private void t曲リストの構築()
 		{
@@ -200,7 +142,7 @@ namespace TJAPlayer3
 
 			try
 			{
-				#region [ 0) システムサウンドの構築  ]
+		#region [ 0) システムサウンドの構築  ]
 				//-----------------------------
 				base.eフェーズID = CStage.Eフェーズ.起動0_システムサウンドを構築;
 
@@ -244,7 +186,7 @@ namespace TJAPlayer3
 					Trace.Unindent();
 				}
 				//-----------------------------
-				#endregion
+		#endregion
 
 				if( CDTXMania.bコンパクトモード )
 				{
@@ -252,7 +194,7 @@ namespace TJAPlayer3
 					return;
 				}
 
-				#region [ 00) songlist.dbの読み込みによる曲リストの構築  ]
+		#region [ 00) songlist.dbの読み込みによる曲リストの構築  ]
 				//-----------------------------
 				base.eフェーズID = CStage.Eフェーズ.起動00_songlistから曲リストを作成する;
 
@@ -293,9 +235,9 @@ namespace TJAPlayer3
 					Trace.Unindent();
 				}
 
-				#endregion
+		#endregion
 
-				#region [ 1) songs.db の読み込み ]
+		#region [ 1) songs.db の読み込み ]
 				//-----------------------------
 				base.eフェーズID = CStage.Eフェーズ.起動1_SongsDBからスコアキャッシュを構築;
 
@@ -336,7 +278,7 @@ namespace TJAPlayer3
 					Trace.Unindent();
 				}
 				//-----------------------------
-				#endregion
+		#endregion
 
 			}
 			finally
