2dc0f6312fe61deed3ce88feab9822297c0e75a9
269Seahorse <51509383+269Seahorse@users.noreply.github.com>
2020-11-06 22:57:08 +0800

    Score fixed

--- TJAPlayer3/Stages/07.Game/CStage演奏画面共通.cs
+++ TJAPlayer3/Stages/07.Game/CStage演奏画面共通.cs
@@ -1781,12 +1781,12 @@ namespace TJAPlayer3
 						//int nBallonNoteCount = CDTXMania.DTX.n風船数[ 2 ] + CDTXMania.DTX.n風船数[ 3 ];
 						//int test = ( 1000000 - ( ( nBallonCount - nBallonNoteCount * 300 ) + ( nBallonNoteCount * 5000 ) ) ) / ( CDTXMania.DTX.nノーツ数[ 2 ] + CDTXMania.DTX.nノーツ数[ 3 ] );
 					}
-
+                                        */
 					if (eJudgeResult == E判定.Great || eJudgeResult == E判定.Good)
 					{
-						nAddScore = nAddScore / 2;
+						nAddScore = nAddScoreNiji / 2;
 					}
-
+                                        /*
 					if (pChip.nチャンネル番号 == 0x13 || pChip.nチャンネル番号 == 0x14 || pChip.nチャンネル番号 == 0x1A || pChip.nチャンネル番号 == 0x1B)
 					{
 						nAddScore = nAddScore * 2;
